1d90a4d335a94d1539e78f2548fd2686f0ec66b4
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-28 18:59:36 +0200

    registered apps

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest-framework',
+    'user',
+    'club'
 ]
 
 MIDDLEWARE = [
