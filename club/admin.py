1ae38f40d7d0b8b9bb3cf02edeb0a42a1b04ade2
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-28 19:14:17 +0200

    Created district

--- club/admin.py
+++ club/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from club.models import Province
+from club.models import District, Province
 
 # Register your models here.
 admin.site.register(Province)
+admin.site.register(District)
--- club/models.py
+++ club/models.py
@@ -1,3 +1,4 @@
+import re
 from django.db import models
 
 # Create your models here.
@@ -7,4 +8,10 @@ class Province(models.Model):
     def __str__(self):
         return self.name
 
+class District(models.Model):
+    name = models.CharField(max_length=100)
+    province = models.ForeignKey(Province, on_delete=models.CASCADE, related_name='districts') 
 
+    def __str__(self):
+        return self.name
+        
